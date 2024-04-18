38c25c12a9febc5c8a3480b0b6c8715ba6176623
ElenaDiab <elena.soave@edu.immaginazioneelavoro.it>
2024-04-18 12:40:58 +0200

    fix card dinamiche

--- js/script-carrello.js
+++ js/script-carrello.js
@@ -27,3 +27,48 @@ function recuperaDestinazioniScelte(){
 }
 
 window.addEventListener("DOMContentLoaded", recuperaDestinazioniScelte)
+
+
+
+
+
+
+
+
+/* ------------------------------ SCRIPT INDEX ------------------------------ */
+// function creaCardDest(destinazione) {
+//     let card = document.createElement("div");
+//     card.setAttribute("class", "card col-3 p-2");
+
+//     let img = document.createElement("img");
+//     img.setAttribute("src", destinazione.locandina);
+//     img.setAttribute("class", "card-img-top")
+//     img.setAttribute("alt", destinazione.titolo)
+
+//     let divCardBody = document.createElement("div");
+//     divCardBody.setAttribute("class", "card-body");
+
+//     card.appendChild(img);
+//     card.appendChild(divCardBody);
+
+//     divCardBody.innerHTML +=  `<h4 class="card-title"> ${destinazione.titolo} </h4>`
+//     divCardBody.innerHTML += `<p class="card-text"> Prezzo Last Minute: ${destinazione.prezzo} € </p>`
+
+//     if (destinazione.disponibilita) {
+//         divCardBody.innerHTML += `<p class="card-text"> Periodo: ${destinazione.periodo} </p>`
+
+//         let buttonAcquista = document.createElement("button");
+//         buttonAcquista.setAttribute("class", "btn btn-primary mt-3");
+//         buttonAcquista.textContent = "Acquista"
+
+//         buttonAcquista.addEventListener("click", function () {
+//             localStorage.setItem(destinazione.id, destinazione.titolo + " - " + destinazione.periodo + " - " + destinazione.prezzo);
+
+//         })
+
+//         divCardBody.appendChild(buttonAcquista);
+//     }else{
+//         divCardBody.innerHTML += `<p class="card-text"> Periodo: Non disponibile </p>`
+//     }
+//     return card;
+// }
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let elDestinazioniLastMin = document.querySelector("#elDestinazioniLastMin")
+let rowContainer = document.querySelector("#elDestinazioniLastMin")
 
 function recuperaDestinazioni(){
     const URLdestinazioni = "http://localhost:3000/destinazioni"
@@ -11,7 +11,8 @@ function recuperaDestinazioni(){
 
             destinazioni.forEach(destinazione => {
                 if (destinazione.disponibilita) {
-                    elDestinazioniLastMin.appendChild(creaCardDest(destinazione));
+                    let colonna = creaColonna(destinazione)
+                    rowContainer.appendChild(colonna);
 
                 }
             });
@@ -26,39 +27,77 @@ window.addEventListener("DOMContentLoaded", recuperaDestinazioni);
  */
 
 
-function creaCardDest(destinazione) {
-    let card = document.createElement("div");
-    card.setAttribute("class", "card col-3 p-2");
+function creaCard(destinazione) {
+    // Crea gli elementi della card
+    const card = document.createElement('div');
+    card.classList.add('card');
 
-    let img = document.createElement("img");
-    img.setAttribute("src", destinazione.locandina);
-    img.setAttribute("class", "card-img-top")
-    img.setAttribute("alt", destinazione.titolo)
+    const imgTop = document.createElement('img');
+    imgTop.classList.add('card-img-top');
+    imgTop.src = destinazione.locandina; 
+    imgTop.alt = 'Immagine destinazione Last Minute';
 
-    let divCardBody = document.createElement("div");
-    divCardBody.setAttribute("class", "card-body");
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
 
-    card.appendChild(img);
-    card.appendChild(divCardBody);
+    const cardTitle = document.createElement('h5');
+    cardTitle.classList.add('card-title');
+    cardTitle.textContent = destinazione.titolo;
 
-    divCardBody.innerHTML +=  `<h4 class="card-title"> ${destinazione.titolo} </h4>`
-    divCardBody.innerHTML += `<p class="card-text"> Prezzo Last Minute: ${destinazione.prezzo} € </p>`
+    const cardText = document.createElement('p');
+    cardText.classList.add('card-text');
+    // cardText.textContent = 'Prezzo Last Minute: ' + destinazione.prezzo + " €";
+
+    
+
+    // Aggiungi gli elementi alla struttura della card
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(cardText);
+    // cardBody.appendChild(button);
+    card.appendChild(imgTop);
+    card.appendChild(cardBody);
+
 
     if (destinazione.disponibilita) {
-        divCardBody.innerHTML += `<p class="card-text"> Periodo: ${destinazione.periodo} </p>`
+        cardBody.innerHTML += `<p class="card-text"> Periodo: ${destinazione.periodo} </p> <p> Prezzo Last Minute:  ${destinazione.prezzo} €`
+        
+        const button = document.createElement('button');
+        button.classList.add('btn')
+        button.classList.add('btn-primary')
+        button.textContent = "Acquista"
+        cardBody.appendChild(button)
 
-        let buttonAcquista = document.createElement("button");
-        buttonAcquista.setAttribute("class", "btn btn-primary mt-3");
-        buttonAcquista.textContent = "Acquista"
-
-        buttonAcquista.addEventListener("click", function () {
-            localStorage.setItem(destinazione.id, destinazione.titolo + " - " + destinazione.periodo + " - " + destinazione.prezzo);
-
+        button.addEventListener("click", function(){
+            localStorage.setItem(destinazione.id, destinazione.titolo + " - " + destinazione.periodo + " - " + destinazione.prezzo)
         })
+         
 
-        divCardBody.appendChild(buttonAcquista);
+        
     }else{
-        divCardBody.innerHTML += `<p class="card-text"> Periodo: Non disponibile </p>`
+        cardBody.innerHTML += `<p class="card-text"> Periodo: Non disponibile </p>`
     }
+
     return card;
-}
+  }
+
+    function creaColonna(destinazione){
+    const colonna = document.createElement('div');
+    colonna.classList.add('col-3');
+
+    // Crea la card
+    const card = creaCard(destinazione);
+
+    // Aggiungi la card alla colonna
+    colonna.appendChild(card);
+
+    return colonna;
+
+  }
+
+
+
+    destinazioni.forEach(function(destinazione) {
+    let colonna = creaColonna(destinazione);
+    rowContainer.appendChild(colonna)
+  })
+
